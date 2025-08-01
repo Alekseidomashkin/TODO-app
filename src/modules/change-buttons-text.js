ab7e3f5d02c5da8ca18418e1cf6d753a77910fdc
Alex Domashkin <alexdomashkin@gmail.com>
2025-08-01 09:23:09 +0300

    LAST VERSION

--- src/modules/change-buttons-text.js
+++ src/modules/change-buttons-text.js
@@ -3,24 +3,21 @@ export const changeButtonsText = function () {
   const formEditTaskButtonElement = document.querySelector(selectors.formEditButton);
   const formCreateTaskButtonElement = document.querySelector(selectors.formCreateButton);
   const deleteAllTasksButtonElement = document.querySelector(selectors.deleteAll);
-  console.log(
-    formEditTaskButtonElement,
-    formCreateTaskButtonElement,
-    deleteAllTasksButtonElement
-  );
   const screenWidth = window.innerWidth;
   if (screenWidth < 515) {
     deleteAllTasksButtonElement.innerText = "Удалить";
-  } else {
-    deleteAllTasksButtonElement.innerText = "Удалить все";
   }
+  // } else {
+  //   deleteAllTasksButtonElement.innerText = "Удалить все";
+  // }
   if (screenWidth < 490) {
     formCreateTaskButtonElement.innerText = "+";
     formEditTaskButtonElement.innerText = "+";
-  } else {
-    formCreateTaskButtonElement.innerText = "Добавить";
-    formEditTaskButtonElement.innerText = "Изменить";
   }
+  // } else {
+  //   formCreateTaskButtonElement.innerText = "Добавить";
+  //   formEditTaskButtonElement.innerText = "Изменить";
+  // }
   if (screenWidth < 471) {
     deleteAllTasksButtonElement.innerText = "-";
   }
